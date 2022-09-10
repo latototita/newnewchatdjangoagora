bd6614e0e16f5890c6ab7b0f4f020c9e01b7d84e
latototita <pearlmartbusinesses@gmail.com>
2022-09-10 11:11:18 -0500

    Laban

--- static/js/streams.js
+++ static/js/streams.js
@@ -28,10 +28,10 @@ let joinAndDisplayLocalStream = async () => {
 
     let member = await createMember()
 
-    let player = `<div style="max-width: 500px; onclick="changec()"
-  border: 3px solid #73AD21;" class="video-container " id="user-container-${UID}">
+    let player = `<div style="max-width: 500px;
+  border: 3px solid #73AD21;" class="video-container col-lg-3 col-md-4 col-sm-6" id="user-container-${UID}">
                      <div style="max-width: 500px;
-  border: 3px solid #73AD21;" onclick="changec()" class="video-player " id="user-${UID}"></div>
+  border: 3px solid #73AD21;"  class="video-player col-lg-3 col-md-4 col-sm-6" id="user-${UID}"></div>
                      <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
                   </div>
                   `
@@ -54,12 +54,12 @@ let handleUserJoined = async (user, mediaType) => {
         let member = await getMember(user)
 /*col-lg-3 col-md-4 col-sm-6*/
         player = `
-        <div  style="max-width: 500px;
+        <div  style="max-width: 500px; 
   
-  border: 3px solid #73AD21;" onclick="changec()" class="video-container " id="user-container-${user.uid}">
+  border: 3px solid #73AD21;"  class="video-container col-lg-3 col-md-4 col-sm-6" id="user-container-${user.uid}">
             <div style="max-width: 500px;
   
-  border: 3px solid #73AD21;" onclick="changec()"  class="video-player " id="user-${user.uid}"></div>
+  border: 3px solid #73AD21;"   class="video-player col-lg-3 col-md-4 col-sm-6" id="user-${user.uid}"></div>
             <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
         </div>
         `
