d69b18013001623196f619ea175c1a1e23e429a4
latototita <pearlmartbusinesses@gmail.com>
2022-09-10 09:31:52 -0500

    Laban

--- static/js/streams.js
+++ static/js/streams.js
@@ -31,7 +31,7 @@ let joinAndDisplayLocalStream = async () => {
     let player = `<div style="max-width: 500px;
   border: 3px solid #73AD21;" class="video-container" id="user-container-${UID}">
                      <div style="max-width: 500px;
-  border: 3px solid #73AD21; " class="video-player" id="user-${UID}"></div>
+  border: 3px solid #73AD21;" class="video-player" id="user-${UID}"></div>
                      <div class="username-wrapper"><span class="user-name">${member.name}</span></div>
                   </div>
                   `
