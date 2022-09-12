09dc092c04b51e814209adb276e24fa863bf90ce
latototita <pearlmartbusinesses@gmail.com>
2022-09-12 08:22:44 -0500

    Laban

--- base/views.py
+++ base/views.py
@@ -66,3 +66,56 @@ def deleteMember(request):
     )
     member.delete()
     return JsonResponse('Member deleted', safe=False)
+
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from Main.models import *
+from Main.forms import *
+
+# Create your views here.
+def signin(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user=authenticate(request, username=username,password=password)
+
+        if user is not None:
+            login(request,user)
+            request.session['customer'] = user.id
+            if 'next' in request.POST:
+                return redirect(request.POST['next'])
+            else:
+                messages.success(request, f'Welcome, {username}.You have Signed In Successfully')
+                return redirect('dashboard')
+        else:
+            messages.success(request, 'Username or Password Incorrect!')
+            context={}
+            return render(request,'sign-in.html',context)
+    context={}
+    return render(request,'sign-in.html',context)
+def signup(response):
+    if response.method=="POST":
+        form=CustomUserCreationForm(response.POST)
+        if form.is_valid():
+            if User.objects.filter(email=form.cleaned_data['email']):
+                messages.success(response, f'Email already in use, Please use a different Email')
+                return render(response,'signup.html',)
+            elif User.objects.filter(username=form.cleaned_data['username']):
+                messages.success(response, f'Username already in use, Please use a different Username')
+                return render(response,'signup.html',)
+            form.save()
+            messages.success(response, f'Successfully Registered,Please log into your Account to Make Orders')
+            return redirect('login')
+    else:
+        form=CustomUserCreationForm()
+    context={'form':form}
+    return render(response,'sign-up.html',context)
+
+
+@login_required(login_url='login')
+def Logout(request):
+    logout(request)
+    messages.success(request, 'You have Signed Out Successfully')
+    return redirect('profile')
