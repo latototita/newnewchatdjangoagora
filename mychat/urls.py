04b3b9a19789bc67f45502f394fb609239da7989
latototita <pearlmartbusinesses@gmail.com>
2022-09-27 04:13:42 -0500

    Laban

--- mychat/urls.py
+++ mychat/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include,re_path
 from django.views.static import serve
 from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
